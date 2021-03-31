63b41a28305a5bbeb042afe82586412ce6cbf42e
category1017 <category1017@gmail.com>
2021-03-31 16:51:12 +0900

    관리자단 권한관리 등록,삭제 구현 & SQL문법 오류 수정

--- src/main/java/edu/human/com/authorrole/service/impl/AuthorRoleServiceImpl.java
+++ src/main/java/edu/human/com/authorrole/service/impl/AuthorRoleServiceImpl.java
@@ -32,4 +32,15 @@ public class AuthorRoleServiceImpl implements AuthorRoleService {
 		authorRoleDAO.updateAuthorRole(authorRoleVO);
 	}
 
+	@Override
+	public void insertAuthorRole(AuthorRoleVO authorRoleVO) throws Exception {
+		authorRoleDAO.insertAuthorRole(authorRoleVO);
+	}
+
+	@Override
+	public void deleteAuthorRole(int AUTHORROLE_ID) throws Exception {
+		authorRoleDAO.deleteAuthorRole(AUTHORROLE_ID);
+		
+	}
+
 }
